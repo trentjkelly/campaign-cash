424b149d886db2a89b4ee220d8321970c6eb3086
Trent Kelly <trentjkelly@outlook.com>
2024-02-05 11:55:01 -0600

    'Trent' Biden UI update p2

--- src/app/trent/politician.tsx
+++ src/app/trent/politician.tsx
@@ -1,8 +1,8 @@
 export default function Politician() {
     return (
-        <div className="relative bg-gray-400 h-64 my-10 mx-20 flex flex-row rounded-xl">
+        <div className="relative bg-gray-400 h-64 my-10 mx-64 flex flex-row rounded-xl">
             <div className="flex flex-row basis-1/3 relative content-center justify-center">
-                <img src="/Joe_Biden_presidential_portrait.jpg" alt="Joe Biden" style={{ width: "auto", height: "auto", maxWidth: "100%", maxHeight: "100%" }} className="rounded-xl" />
+                <img src="/Joe_Biden_presidential_portrait.jpg" alt="Joe Biden" style={{ width: "auto", height: "auto", maxWidth: "100%", maxHeight: "100%" }} className="rounded-3xl my-2" />
             </div>
             <div className="flex flex-col basis-2/3 h-full text-center items-center justify-center">
                 <h1 className="text-7xl">Joe Biden</h1>
