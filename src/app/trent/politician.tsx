17a3917f783346b3e8ac09932c8dd1dd69f4c978
Trent Kelly <trentjkelly@outlook.com>
2024-02-05 09:19:13 -0600

    Joe banner changed in 'Trent'

--- src/app/trent/politician.tsx
+++ src/app/trent/politician.tsx
@@ -1,9 +1,12 @@
 export default function Politician() {
     return (
         <div className="relative bg-gray-400 h-64 my-10 mx-20 flex flex-row rounded-xl">
-            <div className="flex basis-1/3 relative">
-                <img src="/Joe_Biden_presidential_portrait.jpg" alt="Joe Biden" style={{ width: "100%", height: "auto" }} />
+            <div className="flex flex-row basis-1/3 relative content-center justify-center">
+                <img src="/Joe_Biden_presidential_portrait.jpg" alt="Joe Biden" style={{ width: "auto", height: "auto", maxWidth: "100%", maxHeight: "100%" }} className="rounded-xl" />
             </div>
-            <div className="basis-2/3 h-full text-center"><p>Joe Biden</p></div>
+            <div className="flex flex-col basis-2/3 h-full text-center items-center justify-center">
+                <h1 className="text-7xl">Joe Biden</h1>
+                <p>Running for: U.S. President</p>
+            </div>
         </div>)
 }
